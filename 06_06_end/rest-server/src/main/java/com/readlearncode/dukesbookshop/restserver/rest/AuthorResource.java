9ebbe6d05651b6c3d0d0c27caa8b14fc07a04650
pl3298 <53178719+pl3298@users.noreply.github.com>
2019-08-07 09:15:49 +0530

    Add files via upload

---
+++ 06_06_end/rest-server/src/main/java/com/readlearncode/dukesbookshop/restserver/rest/AuthorResource.java
@@ -0,0 +1,31 @@
+package com.readlearncode.dukesbookshop.restserver.rest;
+
+import com.readlearncode.dukesbookshop.restserver.infrastructure.AuthorRepository;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+@Stateless
+@Path("/authors")
+public class AuthorResource {
+
+    @EJB
+    private AuthorRepository authorRepository;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllAuthors(){
+        return Response.ok(authorRepository.getAll()).build();
+    }
+
+}
---
+++ 06_06_end/rest-server/src/main/java/com/readlearncode/dukesbookshop/restserver/rest/BookResource.java
@@ -0,0 +1,115 @@
+package com.readlearncode.dukesbookshop.restserver.rest;
+
+import com.readlearncode.dukesbookshop.restserver.domain.Book;
+import com.readlearncode.dukesbookshop.restserver.domain.LinkResource;
+import com.readlearncode.dukesbookshop.restserver.infrastructure.BookRepository;
+import com.readlearncode.dukesbookshop.restserver.infrastructure.exceptions.ISBNNotFoundException;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+@Stateless
+@Path("/books")
+public class BookResource {
+
+    @EJB
+    private BookRepository bookRepository;
+
+    @Context
+    private UriInfo uriInfo;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllBook() {
+        List<Book> books = bookRepository.getAll();
+        for(Book book : books){
+            Link self = Link.fromUri(uriInfo.getBaseUriBuilder()
+                    .path(getClass())
+                    .path(getClass(), "getBookByIsbn")
+                    .build(book.getId()))
+                    .rel("self")
+                    .type("GET")
+                    .build();
+
+            Link delete = Link.fromUri(uriInfo.getBaseUriBuilder()
+                    .path(getClass())
+                    .path(getClass(), "deleteBook")
+                    .build(book.getId()))
+                    .rel("delete")
+                    .type("DELETE")
+                    .build();
+
+            LinkResource selfLink = new LinkResource(self);
+            LinkResource deleteLink = new LinkResource(delete);
+
+            book.addLink(selfLink);
+            book.addLink(deleteLink);
+        }
+        GenericEntity<List<Book>> bookWrapper = new GenericEntity<List<Book>>(books) {
+        };
+        return Response.ok(bookWrapper).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response saveBook(@Valid final Book book) {
+        Book persistedBook = bookRepository.saveBook(book);
+        return Response.status(Response.Status.OK).entity(persistedBook).build();
+    }
+
+    @GET
+    @Path("{isbn: \\d{9}[\\d|X]$}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBookByIsbn(final @PathParam("isbn") String isbn) throws ISBNNotFoundException {
+        Optional<Book> book = bookRepository.getByISBN(isbn);
+        if (book.isPresent()) {
+
+            Link self = Link.fromUri(uriInfo.getBaseUriBuilder()
+                    .path(getClass())
+                    .path(getClass(), "getBookByIsbn")
+                    .build(book.get().getId()))
+                    .rel("self")
+                    .type("GET")
+                    .build();
+
+            Link delete = Link.fromUri(uriInfo.getBaseUriBuilder()
+                    .path(getClass())
+                    .path(getClass(), "deleteBook")
+                    .build(book.get().getId()))
+                    .rel("delete")
+                    .type("DELETE")
+                    .build();
+
+            LinkResource selfLink = new LinkResource(self);
+            LinkResource deleteLink = new LinkResource(delete);
+
+            book.get().addLink(selfLink);
+            book.get().addLink(deleteLink);
+
+            return Response.ok(book.get()).links(self, delete).build();
+        }
+        throw new ISBNNotFoundException();
+    }
+
+
+    @DELETE
+    @Path("{isbn: \\d{9}[\\d|X]$}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteBook(final @PathParam("isbn") String isbn) throws ISBNNotFoundException {
+        return Response.ok(bookRepository
+                .deleteBook(isbn)
+                .orElseThrow(ISBNNotFoundException::new))
+                .build();
+    }
+
+}
